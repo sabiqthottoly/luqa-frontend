18929d8032a95b5b348f5ba355a19c0444bb0aaf
sarathbhaskaran <sarathkaruvanthala@gmail.com>
2024-05-30 21:01:16 +0530

    move ui folder outside to app

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,5 +1,4 @@
-import { lusitana } from '../ui/fonts';
-
+import { lusitana } from "../../ui/fonts";
 
 export default function Page() {
   return <h1 className={`${lusitana.className}`}>Dashboard Page</h1>;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
 import Link from "next/link";
-import { lusitana } from './ui/fonts';
-import styles from './home.module.css';
-import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { lusitana } from "../ui/fonts";
+import styles from "./home.module.css";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 
 export default function Home() {
   return (
@@ -16,7 +16,7 @@ export default function Home() {
           <p
             className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}
           >
-            <strong>Welcome to Acme.</strong> This is the example for the{' '}
+            <strong>Welcome to Acme.</strong> This is the example for the{" "}
             <a href="https://nextjs.org/learn/" className="text-blue-500">
               Next.js Learn Course
             </a>
@@ -32,20 +32,19 @@ export default function Home() {
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
           <Image
-          src="/hero-desktop.png"
-          width={1000}
-          height={500}
-          className="hidden md:block"
-          alt="Screenshots of the dashboard project showing desktop version"
+            src="/hero-desktop.png"
+            width={1000}
+            height={500}
+            className="hidden md:block"
+            alt="Screenshots of the dashboard project showing desktop version"
           />
-            <Image
-          src="/hero-mobile.png"
-          width={560}
-          height={620}
-          className="block md:hidden"
-          alt="Screenshots of the dashboard project showing desktop version"
+          <Image
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className="block md:hidden"
+            alt="Screenshots of the dashboard project showing desktop version"
           />
-
         </div>
       </div>
     </main>
